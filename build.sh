312387dfd865fd6ee40790b909c4e5f3812fe7f7
Vipin Madhavanunni <vipmadha@gmail.com>
2015-12-19 14:11:08 +0100

    Fixing some of html errors

--- build.sh
+++ build.sh
@@ -1,11 +1,49 @@
 #!/bin/bash
 
-rm -rf _site
-rm -rf .sass-cache
+site_test="-t"
+site_run="-r"
 
-#bundle exec jekyll build
-#bundle exec htmlproof ./_site --disable-external
+function rm_site_temp()
+{
+    rm -rf _site
+    rm -rf .sass-cache
+}
 
-#rm -rf tmp
+function site_build()
+{
+    bundle exec jekyll build
+}
 
-bundle exec jekyll serve --trace
+function run_htmlproof()
+{
+    bundle exec htmlproof ./_site --disable-external
+}
+
+function rm_proff_temp()
+{
+    rm -rf tmp
+}
+
+function site_run()
+{
+    bundle exec jekyll serve --trace
+}
+
+if [ "$1" == "$site_test" ]
+then
+    rm_site_temp
+    rm_proff_temp
+    site_build
+    run_htmlproof
+elif [ "$1" == "$site_run" ]
+then
+   rm_site_temp
+   rm_proff_temp
+   site_run
+else
+   echo "    Error in usage ..."
+   echo "    ./build.sh < -t | -r > "
+   echo "    "
+   exit 1
+fi
+      

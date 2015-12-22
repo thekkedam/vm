bbb402ed19c7e6d40a2989c66172dda61099ef5a
Vipin Madhavanunni <vipmadha@gmail.com>
2015-12-22 17:31:20 +0100

    Updated-12-22-2015-17:31

--- _scripts/external_convert.rb
+++ _scripts/external_convert.rb
@@ -1,26 +1,5 @@
 EXTERNAL_POSTS = [
-  {:date => "2014-01-21", :url => "http://engineering.linkedin.com/play/composable-and-streamable-play-apps", :title => "Composable and Streamable Play Apps"},
-  {:date => "2013-12-09", :url => "http://www.quora.com/LinkedIn-6/What-is-the-current-webstack-LinkedIn-uses/answer/Yevgeniy-Brikman?srid=XPv&amp;share=1", :title => "What is the current webstack LinkedIn uses?"},
-  {:date => "2013-12-03", :url => "http://www.quora.com/Play-Framework/In-which-ways-do-you-use-the-play-framework-in-your-company/answer/Yevgeniy-Brikman?srid=XPv&amp;share=1", :title => "In which ways do you use the play framework in your company?"},
-  {:date => "2013-03-31", :url => "http://www.quora.com/Play-Framework/What-are-the-pros-and-cons-of-the-Play-Framework-2-for-a-Java-developer/answer/Yevgeniy-Brikman?srid=XPv&amp;share=1", :title => "What are the pros and cons of Play Framework 2, for a Java developer?"},
-  {:date => "2013-03-30", :url => "http://www.quora.com/Play-Framework/What-are-the-pros-and-cons-of-Play-Framework-2-for-a-Scala-developer/answer/Yevgeniy-Brikman?srid=XPv&amp;share=1", :title => "What are the pros and cons of Play Framework 2, for a Scala developer?"},
-  {:date => "2013-03-27", :url => "http://engineering.linkedin.com/play/play-framework-async-io-without-thread-pool-and-callback-hell", :title => "Play Framework: async I/O without the thread pool and callback hell"},
-  {:date => "2013-02-20", :url => "http://engineering.linkedin.com/play/play-framework-linkedin", :title => "The Play Framework at LinkedIn"},
-  {:date => "2012-10-01", :url => "http://www.quora.com/Web-Development/What-are-the-pros-and-cons-of-client-side-rendering/answer/Yevgeniy-Brikman?srid=XPv&amp;share=1", :title => "What are the pros and cons of client-side rendering?"},
-  {:date => "2011-06-12", :url => "http://engineering.linkedin.com/23/linkedin-hackdays", :title => "LinkedIn Hackdays"},
-  {:date => "2011-06-12", :url => "http://engineering.linkedin.com/21/coffeescript-javascript-that%E2%80%99s-easy-eyes-0", :title => "CoffeeScript: JavaScript that’s easy on the eyes"},
-  {:date => "2011-04-18", :url => "http://opensource.com/life/11/4/i-finally-understand-open-source-software", :title => "I finally understand open source software"},
-  {:date => "2011-01-15", :url => "http://www.quora.com/Does-the-computer-programming-profession-have-a-future/answer/Yevgeniy-Brikman?srid=XPv&amp;share=1", :title => "Does the computer programming profession have a future?"},
-  {:date => "2015-04-09", :url => "http://www.quora.com/What-does-it-mean-for-a-software-company-startup-to-be-able-to-iterate-quickly/answer/Yevgeniy-Brikman", :title => "What does it mean for a software company/startup to be able to iterate quickly?"},
-  {:date => "2015-03-21", :url => "http://www.quora.com/In-a-startup-how-does-equity-work/answer/Yevgeniy-Brikman", :title => "In a startup, how does equity work?"},
-  {:date => "2015-03-06", :url => "http://www.quora.com/What-are-some-good-books-to-read-for-starting-entrepreneurs/answer/Yevgeniy-Brikman", :title => "What are some good books to read for starting entrepreneurs?"},
-  {:date => "2015-02-19", :url => "http://www.quora.com/How-do-you-make-sure-that-someone-doesnt-steal-a-startup-idea-that-you-are-in-the-initial-stages-of-working-on/answer/Yevgeniy-Brikman", :title => "How do you make sure that someone doesn't steal a startup idea that you are in the initial stages of working on?"},
-  {:date => "2015-02-12", :url => "http://www.quora.com/Should-I-start-a-company-or-work-for-one/answer/Yevgeniy-Brikman", :title => "Should I start a company or work for one?"},
-  {:date => "2015-02-27", :url => "http://www.quora.com/How-can-you-validate-an-idea-before-jumping-in-and-actually-implementing-it/answer/Yevgeniy-Brikman", :title => "How can you validate an idea before jumping in and actually implementing it?"},
-  {:date => "2015-02-12", :url => "http://www.quora.com/What-are-the-best-server-monitor-tools/answer/Yevgeniy-Brikman", :title => "What are the best server monitor tools?"},
-  {:date => "2015-02-12", :url => "http://www.quora.com/How-does-one-get-a-job-at-a-startup/answer/Yevgeniy-Brikman", :title => "How does one get a job at a startup?"},
-  {:date => "2015-02-17", :url => "http://www.quora.com/What-do-you-give-a-child-as-good-reasons-for-why-he-she-should-read/answer/Yevgeniy-Brikman", :title => "What do you give a child as good reasons for why he/she should read?"},
-  {:date => "2015-03-02", :url => "https://www.reddit.com/r/cscareerquestions/comments/2xmn7n/need_advice_should_i_join_a_promising_well_funded/cp1nxmo", :title => "Need advice: Should I join a promising, well funded startup or accept an offer from a Silicon Valley company?"}
+  {:date => "2015-03-02", :url => "https://testweb.com/testlink", :title => "Some title"}
 ]
 
 POST_TEMPLATE = <<eos
